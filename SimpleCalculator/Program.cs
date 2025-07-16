f52b475d98767a2d64014ac98ffeb39a07932df1
Rene Miranda <remirand@itron.com>
2025-07-16 15:31:50 -0400

    Initial commit

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace SimpleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //INPUTS
+            double firstNum = 0, secondNum = 0, sum;
+
+            Console.WriteLine("Enter a the first whole numer:");
+            firstNum = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a the second whole numer:");
+            secondNum = double.Parse(Console.ReadLine());
+
+            //Process
+            sum = firstNum + secondNum;
+
+            //OUTPUTS
+            //String concatination
+            //Console.WriteLine($"The total sum of" + firstNum + "and" + secondNum ":" + sum");
+            //String interpolation: $ indicates that you'll use interpolation
+            Console.WriteLine($"The total sum of {firstNum} and {secondNum}: {sum}");
+            Console.ReadKey();
+        }
+    }
+}
